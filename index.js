f8315099e16399f58c67b445da650d470e545267
Gayan <gayansanjiwa0129@gmail.com>
2025-09-22 12:01:48 +0530

    feat: add to-regex-range library for regex-compatible range matching
    - Implemented the `to-regex-range` module to generate regex patterns from numeric ranges.
    - Added package.json for `to-regex-range` with necessary metadata and dependencies.
    
    feat: introduce touch library for file timestamp manipulation
    
    - Added `touch` module to mimic Unix `touch` command functionality in Node.js.
    - Implemented both async and sync methods for updating file timestamps.
    - Included CLI support for the `nodetouch` command.
    
    chore: add undefsafe library for safe property access in objects
    
    - Introduced `undefsafe` module to safely retrieve and set deep object properties without errors.
    - Added example usage and documentation for clarity.
    - Included tests and configuration files for CI/CD integration.

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import express from  "express"; //imorot express 
 
-let app = express();
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res. json({
+        massage: "this is ged requwest"
+    })
+    console.log("this is a get requwest")
+})
+
+app.delete("/",()=>{
+    console.log("this is delect requwest...")
+})
+
+app.post("/",(req,res)=>{
+    console.log("this is post requwest...")
+})
+
+app.listen( 3000 ,()=>{
+    console.log("server is runnig port 3000 he....")
+})
 
